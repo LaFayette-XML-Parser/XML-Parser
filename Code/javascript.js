dd736ce0b802bf066af38d8d938a6999ee7b477e
LaFayette Engineering <143819193+LaFayette-XML-Parser@users.noreply.github.com>
2024-06-12 14:38:53 -0400

    Update javascript.js

--- Code/javascript.js
+++ Code/javascript.js
@@ -135,12 +135,14 @@ propertiesElements.forEach(propertiesElement => {
     });
 
     // Adjust length and rail centers based on hasCloserollers and model containing 'c' for curve.
-        if (hasCloserollers || /c/i.test(modelValue)) {
-            lengthValue = '0';
+    if(/c/i.test(modelValue)){
+        lengthValue = '0';
+        if (hasCloserollers) {
             railValue = '0 ft 2 in';
         } else if (!hasCloserollers) {
             railValue = '0 ft 3 in';
         }
+    }
 
     // Generate a unique key for this combination of properties to check for repeated lines.
     const combinationKey = `${markedAttribute}_${modelValue}_${priceValue}`;

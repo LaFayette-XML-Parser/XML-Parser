3a758c08d1ec09254776c7fcfed1f498d2a01183
LaFayette Engineering <143819193+LaFayette-XML-Parser@users.noreply.github.com>
2024-06-24 14:22:11 -0400

    Update javascript.js

--- Code/javascript.js
+++ Code/javascript.js
@@ -1,31 +1,43 @@
-      // Formats inches to feet and inches
-        function formatInchesToFeetAndInches(inches) {
-            const feet = Math.floor(inches / 12);
-            let remainingInches = inches % 12;
-            remainingInches = parseFloat(remainingInches.toFixed(2));
-            if (remainingInches >= 12) {
-                feet += 1;
-                remainingInches -= 12;
-            }
-            return `${feet} ft ${remainingInches} in`;
-        }
+// Formats inches to feet and inches
+function formatInchesToFeetAndInches(inches) {
+      
+      const feet = Math.floor(inches / 12);
+      let remainingInches = inches % 12;
+      remainingInches = parseFloat(remainingInches.toFixed(2));
+      if (remainingInches >= 12) {
+            
+            feet += 1;
+            remainingInches -= 12;
+      }
+      return `${feet} ft ${remainingInches} in`;
+}
 
-        // Add event listener for the file input to display file name and enable process button
-document.getElementById('xmlFileInput').addEventListener('change', () => {const fileInput = document.getElementById('xmlFileInput');
-            const fileLabel = document.getElementById('file-input');
-            const fileName = fileInput.files[0].name;
-            fileLabel.querySelector('span').textContent = fileName;
-            document.getElementById('processButton').disabled = false;});
+// Add event listener for the file input to display file name and enable process button
+document.getElementById('xmlFileInput').addEventListener('change', () => {
+      
+      const fileInput = document.getElementById('xmlFileInput');
+      const fileLabel = document.getElementById('file-input');
+      const fileName = fileInput.files[0].name;
+      fileLabel.querySelector('span').textContent = fileName;
+      document.getElementById('processButton').disabled = false;});
 
-        // When button clicked, trigger file input click
-        document.querySelector('.btn').addEventListener('click', () => {document.getElementById('xmlFileInput').click();});
+      // When button clicked, trigger file input click
+      document.querySelector('.btn').addEventListener('click', () => {
+            
+            document.getElementById('xmlFileInput').click();});
 
-        // When the process button is clicked, process the selected XML file
-document.getElementById('processButton').addEventListener('click', async () => {const fileInput = document.getElementById('xmlFileInput');
+      // When the process button is clicked, process the selected XML file
+      document.getElementById('processButton').addEventListener('click', async () => {
+            
+            const fileInput = document.getElementById('xmlFileInput');
             const downloadLink = document.getElementById('downloadLink');
 
             // Ensure a file is selected
-            if (fileInput.files.length === 0) {alert('Please select an XML file.'); return;}
+            if (fileInput.files.length === 0) {
+                  alert('Please select an XML file.'); 
+                  return;
+            }
+                                                                                      
             // Parse the XML file
             const xmlFile = fileInput.files[0];
             const xmlData = await xmlFile.text();
@@ -63,63 +75,64 @@ document.getElementById('processButton').addEventListener('click', async () => {
                 weightValue = '';
 
                 // Parse through each ref name and find the value to record
-refNameElements.forEach(refNameElement => {const propertyName = refNameElement.textContent;
-                    const valueElement = refNameElement.nextElementSibling;
+                refNameElements.forEach(refNameElement => {
+                      const propertyName = refNameElement.textContent;
+                      const valueElement = refNameElement.nextElementSibling;
 
-                    // Assign values based on property name
-                    if (propertyName === 'MarkNumber') {
-                        markedAttribute = valueElement.textContent;
-                    } else if (propertyName === 'Model') {
-                        modelValue = valueElement.textContent;
-                    } else if (propertyName === 'iopcount') {
-                        iopCountValue = valueElement.textContent;
-                    } else if (propertyName === 'overallwidth') {
-                        widthValue = formatInchesToFeetAndInches(parseFloat(valueElement.textContent));
-                    } else if (propertyName === 'rollercenters') {
-                        railValue = formatInchesToFeetAndInches(parseFloat(valueElement.textContent));
-                    } else if (propertyName === 'curveangle') {
-                        curveValue = valueElement.textContent;
-                    } else if (propertyName === 'overalllength') {
-                        lengthValue = formatInchesToFeetAndInches(parseFloat(valueElement.textContent));
-                    } else if (propertyName === 'infeedheight') {
-                        infeedValue = formatInchesToFeetAndInches(parseFloat(valueElement.textContent));
-                    } else if (propertyName === 'dischargeheight') {
-                        dischargeValue = formatInchesToFeetAndInches(parseFloat(valueElement.textContent));
-                    } else if (propertyName === 'hp') {
-                        hpValue = parseFloat(valueElement.textContent).toFixed(2);
-                    } else if (propertyName === 'fpm') {
-                        speedValue = parseFloat(valueElement.textContent).toFixed(2);
-                    } else if (propertyName === 'conveyorweight') {
-                        weightValue = parseFloat(valueElement.textContent.trim()).toFixed(2);
-                        if (weightValue === '0.00') {weightValue = '';}
-                    } else if (propertyName === 'powersupplysize') {
-                        const psAmpValue = valueElement.textContent;
+                      // Assign values based on property name
+                      if (propertyName === 'MarkNumber') {
+                          markedAttribute = valueElement.textContent;
+                      } else if (propertyName === 'Model') {
+                          modelValue = valueElement.textContent;
+                      } else if (propertyName === 'iopcount') {
+                          iopCountValue = valueElement.textContent;
+                      } else if (propertyName === 'overallwidth') {
+                          widthValue = formatInchesToFeetAndInches(parseFloat(valueElement.textContent));
+                      } else if (propertyName === 'rollercenters') {
+                          railValue = formatInchesToFeetAndInches(parseFloat(valueElement.textContent));
+                      } else if (propertyName === 'curveangle') {
+                          curveValue = valueElement.textContent;
+                      } else if (propertyName === 'overalllength') {
+                          lengthValue = formatInchesToFeetAndInches(parseFloat(valueElement.textContent));
+                      } else if (propertyName === 'infeedheight') {
+                          infeedValue = formatInchesToFeetAndInches(parseFloat(valueElement.textContent));
+                      } else if (propertyName === 'dischargeheight') {
+                          dischargeValue = formatInchesToFeetAndInches(parseFloat(valueElement.textContent));
+                      } else if (propertyName === 'hp') {
+                          hpValue = parseFloat(valueElement.textContent).toFixed(2);
+                      } else if (propertyName === 'fpm') {
+                          speedValue = parseFloat(valueElement.textContent).toFixed(2);
+                      } else if (propertyName === 'conveyorweight') {
+                          weightValue = parseFloat(valueElement.textContent.trim()).toFixed(2);
+                          if (weightValue === '0.00') {weightValue = '';}
+                      } else if (propertyName === 'powersupplysize') {
+                          const psAmpValue = valueElement.textContent;
                           
-                        // Makes a count of 0 if there is nothing there.
-                        if (psAmpValue.trim() === '') {
-                            psAmpValuesMap.set('', (psAmpValuesMap.get('') || 0) + 1);
+                          // Makes a count of 0 if there is nothing there.
+                          if (psAmpValue.trim() === '') {
+                              psAmpValuesMap.set('', (psAmpValuesMap.get('') || 0) + 1);
                               
                               // Changes less power supply to 0.
-                        } else if (psAmpValue.toLowerCase() !== 'less power supply') {
-                            const ampMatch = psAmpValue.match(/\d+/);
-                            if (ampMatch) {
-                                const amp = ampMatch[0];
-                                psAmpValuesMap.set(amp, (psAmpValuesMap.get(amp) || 0) + 1);
-                            }
-                        }
-                    } else if (propertyName === 'TotalPrice') {
-                        priceValue = parseFloat(valueElement.textContent || '0').toFixed(2);
-                    } else if (propertyName === 'hascloserollers') {
+                          } else if (psAmpValue.toLowerCase() !== 'less power supply') {
+                              const ampMatch = psAmpValue.match(/\d+/);
+                              if (ampMatch) {
+                                  const amp = ampMatch[0];
+                                  psAmpValuesMap.set(amp, (psAmpValuesMap.get(amp) || 0) + 1);
+                              }
+                          }
+                      } else if (propertyName === 'TotalPrice') {
+                          priceValue = parseFloat(valueElement.textContent || '0').toFixed(2);
+                      } else if (propertyName === 'hascloserollers') {
                           
-                        // Makes value lowercase and checks the value inside. If it has the necessary value, its true.
-                        const hasCloserollers = valueElement.textContent.toLowerCase() === 'true';
-                        if (hasCloserollers) {
-                            railValue = '0 ft 2 in';
-                        } else {
-                            railValue = '0 ft 3 in';
-                        }
-                    }
-                });
+                          // Makes value lowercase and checks the value inside. If it has the necessary value, its true.
+                          const hasCloserollers = valueElement.textContent.toLowerCase() === 'true';
+                          if (hasCloserollers) {
+                              railValue = '0 ft 2 in';
+                          } else {
+                              railValue = '0 ft 3 in';
+                          }
+                      }
+      });
 
                 // Makes zeroes blank. Searches for instances.                                             
                 if (/C/.test(modelValue)) {
